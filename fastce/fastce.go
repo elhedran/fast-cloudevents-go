09542b6d9b4a5bf2e5bae9d7cc280d5e6b8b274c
Matt <matt3dixon@gmail.com>
2020-02-04 10:10:16 +1000

    wip/ stashing work on CtxBinaryToCE

--- fastce/fastce.go
+++ fastce/fastce.go
@@ -1,7 +1,7 @@
 package fastce
 
 import (
-	// "encoding/json"
+	"time"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +11,15 @@ import (
 
 	"github.com/valyala/fasthttp"
 )
+
+/*
+  ██████╗ ██████╗ ███╗   ██╗███████╗██╗   ██╗███╗   ███╗███████╗
+ ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║   ██║████╗ ████║██╔════╝
+ ██║     ██║   ██║██╔██╗ ██║███████╗██║   ██║██╔████╔██║█████╗  
+ ██║     ██║   ██║██║╚██╗██║╚════██║██║   ██║██║╚██╔╝██║██╔══╝  
+ ╚██████╗╚██████╔╝██║ ╚████║███████║╚██████╔╝██║ ╚═╝ ██║███████╗
+  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+*/
 
 // GetEvents determines the mode and content type of a request and gets any event(s) from it
 func GetEvents(ctx *fasthttp.RequestCtx) (ces []j.CloudEvent, mode j.Mode, err error) {
@@ -45,16 +54,6 @@ func GetEvents(ctx *fasthttp.RequestCtx) (ces []j.CloudEvent, mode j.Mode, err e
 		err = fmt.Errorf("Unknown mode: %s", mode)
 	}
 	return
-}
-// CtxStructureJSONToCE converts a RequestCtx in Structured mode with JSON content type into a jsonce CloudEvent
-func CtxStructureJSONToCE(ctx *fasthttp.RequestCtx) (ce j.CloudEvent, err error) {
-	body := ctx.PostBody()
-	ce = j.CloudEvent{}
-	err = ce.UnmarshalJSON(body)
-	if err != nil {
-		return ce, fmt.Errorf("Could not unmarshal to event: %s", err.Error())
-	}
-	return ce, err
 }
 
 // CtxBinaryToCE converts a RequestCtx in Binary mode into a jsonce CloudEvent
@@ -94,6 +93,17 @@ func CtxBinaryToCE(ctx *fasthttp.RequestCtx) (ce j.CloudEvent, err error) {
 	return ce, err
 }
 
+// CtxStructureJSONToCE converts a RequestCtx in Structured mode with JSON content type into a jsonce CloudEvent
+func CtxStructureJSONToCE(ctx *fasthttp.RequestCtx) (ce j.CloudEvent, err error) {
+	body := ctx.PostBody()
+	ce = j.CloudEvent{}
+	err = ce.UnmarshalJSON(body)
+	if err != nil {
+		return ce, fmt.Errorf("Could not unmarshal to event: %s", err.Error())
+	}
+	return ce, err
+}
+
 // GetMode uses the Content Type header to determine the content mode of the request
 // https://github.com/cloudevents/spec/blob/v1.0/http-protocol-binding.md#3-http-message-mapping
 func GetMode(ctx *fasthttp.RequestCtx) (mode j.Mode) {
@@ -107,6 +117,94 @@ func GetMode(ctx *fasthttp.RequestCtx) (mode j.Mode) {
 	}
 	return mode
 }
+
+/*
+ ██████╗ ██████╗  ██████╗ ██████╗ ██╗   ██╗ ██████╗███████╗
+ ██╔══██╗██╔══██╗██╔═══██╗██╔══██╗██║   ██║██╔════╝██╔════╝
+ ██████╔╝██████╔╝██║   ██║██║  ██║██║   ██║██║     █████╗  
+ ██╔═══╝ ██╔══██╗██║   ██║██║  ██║██║   ██║██║     ██╔══╝  
+ ██║     ██║  ██║╚██████╔╝██████╔╝╚██████╔╝╚██████╗███████╗
+ ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝  ╚═════╝╚══════╝
+*/
+
+// PutEvents determines the mode and content type of a request and puts any event(s) into it
+// Note that ces[1...] are dropped unless mode is batch
+func PutEvents(ctx *fasthttp.RequestCtx, ces []j.CloudEvent, mode j.Mode) (err error) {
+	if len(ces) <1 {
+		return fmt.Errorf("Could not put %d events", len(ces))
+	}
+
+	// https://github.com/cloudevents/spec/blob/v1.0/http-protocol-binding.md#13-content-modes
+	switch mode {
+	case j.ModeBinary:
+		err := CEToCtxBinary(ctx, ces[0])
+		if err !=nil {
+			return fmt.Errorf("Could not set binary event: %s", err.Error())
+		}
+		return nil
+	case j.ModeStructure:
+		ce := ces[0]
+		ct := ce.DataContentType
+		ctx.Response.Header.Set("Content-Type", ct)
+
+		if !strings.HasPrefix(ct, "application/cloudevents+json") {
+			return fmt.Errorf("Unknown event content media type: %s", ct)
+		}
+
+		err := CEToCtxStructureJSON(ctx, ce)
+		if err !=nil {
+			return fmt.Errorf("Could not set structure event: %s", err.Error())
+		}
+		return nil
+	case j.ModeBatch:
+		err = fmt.Errorf("Unimplemented mode: %s", mode)
+	default:
+		err = fmt.Errorf("Unknown mode: %s", mode)
+	}
+	return
+}
+
+// CEToCtxBinary converts a jsonce CloudEvent into a RequestCtx in Binary mode
+func CEToCtxBinary(ctx *fasthttp.RequestCtx, ce j.CloudEvent) (err error){	
+	// Required
+	ctx.Response.Header.Set("ce-id", ce.Id)
+	ctx.Response.Header.Set("ce-source", ce.Source)
+	ctx.Response.Header.Set("ce-specversion", ce.SpecVersion)
+	ctx.Response.Header.Set("ce-type", ce.Type)
+	// Optional
+	ctx.Response.Header.Set("Content-Type", ce.DataContentType)
+	ctx.Response.Header.Set("ce-dataschema", ce.DataSchema)
+	ctx.Response.Header.Set("ce-subject", ce.Subject)
+	ctx.Response.Header.Set("ce-time", ce.Time.Format(time.RFC3339Nano))
+	// Additional
+	ctx.Write(ce.Data)
+	for k, v := range ce.Extensions {
+		ctx.Response.Header.Set(fmt.Sprintf("ce-%s", k), fmt.Sprintf("%s", v))
+	}
+	return nil
+}
+
+// CEToCtxStructureJSON converts a jsonce CloudEvent into a RequestCtx in Structured mode with JSON content type
+func CEToCtxStructureJSON(ctx *fasthttp.RequestCtx, ce j.CloudEvent) (err error){
+	js, err := ce.MarshalJSON()
+	if err != nil {
+		return fmt.Errorf("Could not marshal event: %s", err.Error())
+	}
+
+	ctx.Response.Header.Set("Content-Type", ce.DataContentType)
+	ctx.Write(js)
+
+	return nil
+}
+
+/*
+ ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ 
+ ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+ ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
+ ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
+ ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
+ ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+*/
 
 // ExampleServer shows an example implementation of each method with a fasthttp server.
 func ExampleServer(listenAddr string) {
@@ -122,61 +220,14 @@ func ExampleServer(listenAddr string) {
 					ctx.Error(err.Error(), http.StatusBadRequest)
 					return
 				} else {
-					log.Printf("OK : Received %s events in mode %s", len(ces), mode)
+					log.Printf("OK : Received %d events in mode %d\n", len(ces), mode)
 				}
 				fmt.Printf("\tData: %#v\n", ces)
-				ctx.Write([]byte(fmt.Sprintf("STUB:NO REPLY: %#v", ces)))
+				// ctx.Write([]byte(fmt.Sprintf("STUB:NO REPLY: %#v", ces)))
+				PutEvents(ctx, ces, mode)
 		}
 	}
 	if err := fasthttp.ListenAndServe(listenAddr, router); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
-	}
-}
-
-// // EventToFastHTTPStructured sets the given headers and body on the given fasthttp response in structured mode.
-// func EventToFastHTTPStructured(event j.CloudEvent) func(*fasthttp.RequestCtx)error {
-// 	return func(ctx *fasthttp.RequestCtx) (err error) {
-// 		ctx.Response.Header.Set("Content-Type", event.Datacontenttype)
-// 	/*
-// 		jsonEvent := map[string][]byte{}
-// 		jsonEvent["type"] = []byte(event.Type)
-// 		jsonEvent["time"] = []byte(event.Time.Format(time.RFC3339))
-// 		jsonEvent["id"] = []byte(event.Id)
-// 		jsonEvent["source"] = []byte(event.Source)
-// 		jsonEvent["specversion"] = []byte(event.Specversion)
-// 		for k, v := range event.Extensions {
-// 			jsonEvent[k] = []byte(v)
-// 		}
-// 		jsonEvent["data"] = []byte(event.Data)
-// 	*/	js, err := json.Marshal(event)
-// 		if err != nil {
-// 			err = fmt.Errorf("Could not marshal: %s", err.Error())
-// 			return
-// 		}
-// 		ctx.Write(js)
-// 		return
-// 	}
-// }
-
-// EventToFastHTTPBinary sets the given headers and body on the given fasthttp response in binary mode.
-func EventToFastHTTPBinary(event j.CloudEvent) func(*fasthttp.RequestCtx)error {
-	return func(ctx *fasthttp.RequestCtx) (err error) {
-		ctx.Response.Header.Set("Content-Type", event.Datacontenttype)
-		ctx.Response.Header.Set("ce-type", event.Type)
-		ctx.Response.Header.Set("ce-time", event.Time.Format(time.RFC3339Nano))
-		ctx.Response.Header.Set("ce-id", event.Id)
-		ctx.Response.Header.Set("ce-source", event.Source)
-		ctx.Response.Header.Set("ce-specversion", event.Specversion)
-		ctx.Write(event.Data)
-		ExtensionsToFastHTTPBinary(ctx, event.Extensions)
-		return
-	}
-}
-
-
-// ExtensionsToFastHTTPBinary sets the given extension headers on the given fasthttp response in binary mode.
-func ExtensionsToFastHTTPBinary(ctx *fasthttp.RequestCtx, extensions map[string]json.RawMessage) {
-	for k, v := range extensions {
-		ctx.Response.Header.Set(fmt.Sprintf("ce-%s", k), string(v))
 	}
 }
